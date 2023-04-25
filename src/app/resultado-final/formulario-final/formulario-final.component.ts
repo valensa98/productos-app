4992276cf1d2a777757a7d2830f6382d3658595e
valensa98 <valesala.1269@gmail.com>
2023-04-25 16:33:17 -0500

    validacionRango

--- src/app/resultado-final/formulario-final/formulario-final.component.ts
+++ src/app/resultado-final/formulario-final/formulario-final.component.ts
@@ -9,7 +9,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class FormularioFinalComponent {
   titulo = 'Formulario para Calcular Notas Finales';
 
-  estudiantes: number[] = [];
+  estudiantes: number = 15;
   nombres: string[] = []; // almacena los nombres de los estudiantes
   nuevoNombre: string = ''; // guarda el nombre que el usuario ingrese
   notasM1: number[] = []; // guarda las notas de la materia 1
@@ -21,7 +21,6 @@ export class FormularioFinalComponent {
   notaFinal: number = 0; //almacena la nota final del estudiante
   notaMateria1: number[] = [];
   notaMateria2: number[] = [];
-
 
   //se agrega el servicio en el constructor
   constructor(private router: Router, private route: ActivatedRoute) {}
@@ -40,17 +39,17 @@ export class FormularioFinalComponent {
       (this.notasM1[0] + this.notasM1[1]) * 0.15 +
       (this.notasM1[2] + this.notasM1[3]) * 0.1 +
       this.notasM1[4] * 0.5;
-      this.notaMateria1.push(this.notaM1);//agrega la nota final de la materia1 a un arreglo
+    this.notaMateria1.push(this.notaM1); //agrega la nota final de la materia1 a un arreglo
 
     this.notaM2 =
       (this.notasM2[0] + this.notasM2[1]) * 0.15 +
       (this.notasM2[2] + this.notasM2[3]) * 0.1 +
       this.notasM2[4] * 0.5;
-      this.notaMateria2.push(this.notaM2);//agrega la nota final de la materia2 a un arreglo
+    this.notaMateria2.push(this.notaM2); //agrega la nota final de la materia2 a un arreglo
   }
 
   calcularNotaFinal() {
     this.notaFinal = (this.notaM1 + this.notaM2) / 2;
-    this.notasFinales.push(this.notaFinal);//agrega la nota final a un arreglo
+    this.notasFinales.push(this.notaFinal); //agrega la nota final a un arreglo
   }
 }
